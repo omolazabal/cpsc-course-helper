9ee98910160a2386e8a6ad2d4ce9e469a7b105c3
Oscar Olazabal <oscarolazabal@gmail.com>
2017-07-25 04:17:19 -0700

    add video games

--- lambda-function/courses.js
+++ lambda-function/courses.js
@@ -1,86 +1,6 @@
 module.exports = {
-    "COURSE_EN_GB" : {
-      
-      // Introductory classes
-      "programming" : "Computer science 120 and 121 introduces the concepts under all computer programming with C++. In 120 you'll cover the basics ranging from input and output to functions. In 121 you'll cover object oriented programming.",
-      "coding" : "Computer science 120 and 121 introduces the concepts under all computer programming with C++. In 120 you'll cover the basics ranging from input and output to functions. In 121 you'll cover object oriented programming.",
-      "see plus plus" : "Computer science 120 and 121 introduces the concepts under all computer programming with C++. In 120 you'll cover the basics ranging from input and output to functions. In 121 you'll cover object oriented programming.",
-      "data structures": "Data structures will be covered in compupter science 131. The data structures that will be covered include vectors, linked list, stack, queue, binary search tree, and graph representations.",
-
-      // Languages
-      "see": "C programming will be taught in computer science 223C.",
-      "visual basic": "You'll learn visual basic in computer science 223H.",
-      "java": "Computer science 233J teaches java programming.",
-      "c sharp": "C sharp programming will be taught in computer science 223N.",
-      "python": "You'll learn how to program in python in computer science 223P.",
-      "javascript" : "You'll learn about javascript in the web front end engineering course, also known as computer science 473",
-
-      // Upper Division
-      "assembly": "In computer science 240 you'll learn about the structure of low level computer languages such as assembly.",
-      "linux": "Computer science 254 covers software development with open source systems, specifically linux.",
-      "technical writing": "You'll learn about technical writing in computer science 311.",
-      "ethics": "Computer science 315 is Professional Ethics in Computing. You'll learn about the ethics and moral philosophy as applied to software and digital artifacts.",
-      "compilers": "Compilers are cover in computer science 323. Topics include the history of programming languages, lagauge design issues ad criteria, and the design of compilers for modern programming languages.",
-      "file structures": "Computer science 332 talks about the fundamental theories and design of datbase systems, SQL, and basic concepts and techniques of data organization.",
-      "sql": "Computer science 332 talks about the fundamental theories and design of database systems, SQL, and basic concepts and techniques of data organization.",
-      "structured query language" : "Computer science 332 talks about the fundamental theories and design of database systems, SQL, and basic concepts and techniques of data organization.",
-      "database systems": "Computer science 332 talks about the fundamental theories and design of datbase systems, SQL, and basic concepts and techniques of data organization.",
-      "algorithms": "Computer Science 335 is Algorithm Engineering. You'll learn about classical patterns such as exhaustive search, divide and conquer, randomization, hashing, dynamic programming, and more.",
-      "algorithm engineering": "Computer Science 335 is Algorithm Engineering. You'll learn about classical patterns such as exhaustive search, divide and conquer, randomization, hashing, dynamic programming, and more.",
-      "operating systems": "You'll learn about operating systems in computer science 351, which goes over Resource management, memory organization, input/output, control process synchronization and other concepts.",
-      "computer architecture": "Computer Architecture is covered in computer science 440. It covers computer performance, instruction set design and examples, processor design, pipelining, memory, and more.",
-      "communications": "Computer science 471 is computer communications. It goes over terminology, networks, common-carrier services, telecommunication facilities, terminals, error control, and multiplexing and concentration techniques.",
-      "artificial intelligence": "Computer science 481 is Artificial Intelligence. In this course you'll learn about using computers to simulate intelligence.",
-      "ai": "Computer science 481 is Artificial Intelligence. In this course you'll learn about using computers to simulate intelligence.",
-
-      //Security
-      "security": "To get an introductory to security, you would take computer science 353 where you'll learn about goals, systems, crytography fundamentals, standards, and much more. In other classes in security, you'll cover crytography, the cloud, and network security.",
-      "cryptography": "Computer science 452 is Cryptography. 452 will cover the various aspects of crytography and steganography such as encrytion, assymetric crytography, and the various types of attacks in the industry.",
-      "cloud": "In computer science 454 you'll learn about cloud computing and security.",
-      "cloud computing": "In computer science 454 you'll learn about cloud computing and security.",
-      "network security": "In computer science 456 you'll learn about the the fundamentals of network security such as the vulerabilities of network protocols, attacks targeting confidentiality, and more.",
-
-      //Internet and enterprise
-      "databases": "You'll learn about database design and application development techniques for real world systems in computer science 431. Topics gone over include: system analysis, requirement specifications, conceptual modeling, and more.",
-      "web apps": "Computer science 473 is web front-end engineering for internet applications. It covers concepts and architecture of interactive web applications.",
-      "web front end engineering": "Computer science 473 is web front-end engineering for internet applications. It covers cencepts and architecture of interactive web applications.",
-      "web back end engineering": "Computer science 476 covers web back end engineering for enterprise applications. In there you'll learn about the concepts and architecture of enterprise applications, components, servises, and communication technologies.",
-      "enterprise applications": "Computer science 476 covers web back end engineering for enterprise applications. In there you'll learn about the concepts and architecture of enterprise applications, components, servises, and communication technologies.",
-      
-      //Multimedia and Digital game Technologies
-      "games": "Computer science 386 is an introductory to game design and production. It will go over current and future game technologies and market trends in game design and production. In other game courses you will go over computer graphics, game programming, and game development.",
-      "computer graphics": "Computer graphics is taught in computer science 484. In the course you will examine and analyze computer graphics, software structures, display processor organization, and much more.",
-      "game programming": "In computer science 486 you will survey data structures and algorithms used for real-time rendering and computer game programming.",
-      "game development": "Game development is taught in computer science 489. You will individually, or in a team, design, plan, and build a computer game.",
-
-      //Scientific Computing
-      "calculus": "MATH 250A is Calculus 3.",
-      "linear algebra": "MATH 250B is Introduction to Linear Algebra and Differential Equations.",
-      "numerical analysis": "MATH 340 is Numerical Analysis.",
-      "mathematical models": "MATH 370 is Mathematical Model Building.",
-
-      //Software Engineering
-      "software engineering": "Computer science 362 is Foundations of Software Engineering and it covers the basic concepts, principles, methods, techniques and practices of software engineering. In other software engineering courses you will cover software testing, architecture, and development.",
-      "software design": "In computer science 462 you will learn about the concepts of software modeling and process for software design.",
-      "software architecture": "Basic principles and practices of software design and architecture are covered in computer science 464.",
-      "software testing": "In computer science 463 you'll learn about software testing techniques, reporting problems effectively, and planning and testing projects.",
-      "software development": "Software Development is computer science 466. This course is a guidance for improving the software development and maintenance process. You will be exposed to common process models such as CMM, CMMI, PSP, and TSP.",
-      
-      //Custom
-      "mobile apps" : "Computer science 411 will introduce you wo developing applications for mobile devices, including runtime enviroments, development tools, and debugging tools.",
-      "apps" : "Computer science 411 will introduce you wo developing applications for mobile devices, including runtime enviroments, development tools, and debugging tools.",
-      "mobile app development" : "Computer science 411 will introduce you to developing applications for mobile devices, including runtime enviroments, development tools, and debugging tools.",
-      "theory of computation" : "Theory of computation is covered in computer science 439. It covers finite state machines, context free grammars, and Turing machines.",
-      "computational theory" : "Theory of computation is covered in computer science 439. It covers finite state machines, context free grammars, and Turing machines.",
-      "parallel computing" : "Computer science 474 will teach you about the concepts of distributed computing; such as distributed memory, parallel programming techniques, and more.",
-      "distributed computing" : "Computer science 474 will teach you about the concepts of distributed computing; such as distributed memory, parallel programming techniques, and more.",
-      "grid computing" : "Grid computing technologies and concepts are taught in computer science 477. Topics include designing grid operating systems, load balancing, distributed hash tables, and more",
-      "data mining" : "Data mining and pattern recognition is taught in computer science 483. You will learn about classification techniques, discriminant functions, training algorithms, supervised and unsupervised learning, and more",
-      "machine learning" : "Data mining and pattern recognition is taught in computer science 483. You will learn about classification techniques, discriminant functions, training algorithms, supervised and unsupervised learning, and more"
-
-  },
     "COURSE_EN_US": {
-  
+
       // Introductory classes
       "programming" : "Computer science 120 and 121 introduces the concepts under all computer programming with C++. In 120 you'll cover the basics ranging from input and output to functions. In 121 you'll cover object oriented programming.",
       "coding" : "Computer science 120 and 121 introduces the concepts under all computer programming with C++. In 120 you'll cover the basics ranging from input and output to functions. In 121 you'll cover object oriented programming.",
@@ -126,8 +46,9 @@ module.exports = {
       "web front end engineering": "Computer science 473 is web front-end engineering for internet applications. It covers cencepts and architecture of interactive web applications.",
       "web back end engineering": "Computer science 476 covers web back end engineering for enterprise applications. In there you'll learn about the concepts and architecture of enterprise applications, components, servises, and communication technologies.",
       "enterprise applications": "Computer science 476 covers web back end engineering for enterprise applications. In there you'll learn about the concepts and architecture of enterprise applications, components, servises, and communication technologies.",
-      
+
       //Multimedia and Digital game Technologies
+      "video games": "Computer science 386 is an introductory to game design and production. It will go over current and future game technologies and market trends in game design and production. In other game courses you will go over computer graphics, game programming, and game development.",
       "games": "Computer science 386 is an introductory to game design and production. It will go over current and future game technologies and market trends in game design and production. In other game courses you will go over computer graphics, game programming, and game development.",
       "computer graphics": "Computer graphics is taught in computer science 484. In the course you will examine and analyze computer graphics, software structures, display processor organization, and much more.",
       "game programming": "In computer science 486 you will survey data structures and algorithms used for real-time rendering and computer game programming.",
